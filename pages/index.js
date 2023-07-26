b29a39b7461e26e4e3d75ac17d541a3e24209b24
adomovec <aleksej.domovec@xim.ag>
2023-07-26 13:54:03 +0200

    ga added

--- pages/index.js
+++ pages/index.js
@@ -102,6 +102,15 @@ function Home({ title, subline, paragraph, button, button_text, tabs, contact, u
   return (
     <>
       <Script id="cookieyes" src="https://cdn-cookieyes.com/client_data/f38bad507948038fa664f720/script.js" strategy="beforeInteractive" />
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-960RQQPD73" />
+      <Script strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-960RQQPD73');
+            `}
+      </Script>
 
       <Head>
         <title>Dev-Kid | Design und Webentwicklung</title>
